45dc6fa128c3049b0327538b57c09d44308ec49e
Jugnu Agrawal <jugnu@appveen.com>
2022-06-14 15:10:49 +0530

    Image Tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ echo "****************************************************"
 echo "data.stack:ne :: Building NE using TAG :: $TAG"
 echo "****************************************************"
 
+sed -i.bak s#__image_tag__#$TAG# Dockerfile
 
 if [ $cleanBuild ]; then
     docker build --no-cache -t data.stack.ne:$TAG .
