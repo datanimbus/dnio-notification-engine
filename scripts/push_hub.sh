8f3c6def9217cd537f09ecfd860b4b57d6dc8159
Jugnu Agrawal <jugnu@appveen.com>
2023-09-20 14:54:10 +0530

    dockerhub change

--- scripts/push_hub.sh
+++ scripts/push_hub.sh
@@ -5,12 +5,12 @@ set -e
 TAG=`cat CURRENT_NE`
 
 echo "****************************************************"
-echo "datanimbus.io.ne :: Pushing Image to Docker Hub :: appveen/datanimbus.io.ne:$TAG"
+echo "datanimbus.io.ne :: Pushing Image to Docker Hub :: datanimbus/datanimbus.io.ne:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.ne:$TAG appveen/datanimbus.io.ne:$TAG
-docker push appveen/datanimbus.io.ne:$TAG
+docker tag datanimbus.io.ne:$TAG datanimbus/datanimbus.io.ne:$TAG
+docker push datanimbus/datanimbus.io.ne:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.ne :: Image Pushed to Docker Hub AS appveen/datanimbus.io.ne:$TAG"
+echo "datanimbus.io.ne :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.ne:$TAG"
 echo "****************************************************"
